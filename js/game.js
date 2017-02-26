5a3c5affc3370ec14ce2a730129f6aa512e59624
AJ Ford <AJCranford1019@gmail.com>
2017-02-26 15:57:50 -0500

    init

---
+++ js/game.js
@@ -0,0 +1,103 @@
+// Page loads
+// Game Initializes 
+// Random Number is Generated
+// Crystals Gain Values 
+
+// User Clicks Crystals 
+// Value Added to Score 
+// If value exceeds random number -- lose
+// If value < random number -- continue
+// If value === random num; win. 
+
+// Add win or Loss 
+// Reset Game 
+
+$(document).ready(function(){
+
+var score = 0;
+
+var randomNum = Math.floor(Math.random() * 31) + 50;
+console.log(randomNum);
+
+// Check if win or lose
+
+ function check(){
+		if(score === randomNum){
+	alert("You win!");
+	resetScore();
+	
+
+} else if(score > randomNum){ 
+	alert("You Lose!");
+
+	}
+}
+
+// Reset Score
+
+function resetScore(){
+	score = 0;
+	$("#random-number").html(randomNum);
+	$("#myScore").html(score);
+
+}
+
+
+// Update Rupee Score
+
+ function update(){ 
+ 	$("#myScore").html(score); 
+ }
+
+$("#random-number").html(randomNum);
+
+// Click Functionality 
+
+$("#rupee-silver").on("click", function(){
+	score ++;
+	update();
+	check();
+});
+
+$("#rupee-red").on("click", function(){
+	score += 5;
+	update();
+	check();
+});
+
+$("#rupee-purple").on("click", function(){
+	score += 2;
+	update();
+	check();
+});
+
+$("#rupee-blue").on("click", function(){
+	score += 10;
+	update();
+	check();
+});
+
+//Reset
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+//End Page Load
+
+});
+
+
